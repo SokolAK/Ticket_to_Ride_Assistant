752f3c84fb76198a1fe1eb398ae3f2518dfdac9b
SokolAK <sokolak87@gmail.com>
2020-11-26 18:59:19 +0100

    Improve 'dimens.xml' files

--- app/src/main/java/pl/sokolak/TicketToRideAssistant/Fragments/HelpActivity.java
+++ app/src/main/java/pl/sokolak/TicketToRideAssistant/Fragments/HelpActivity.java
@@ -2,10 +2,14 @@ package pl.sokolak.TicketToRideAssistant.Fragments;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.widget.Toolbar;
 
+import lombok.SneakyThrows;
 import pl.sokolak.TicketToRideAssistant.R;
 
 public class HelpActivity extends AppCompatActivity {
@@ -13,8 +17,15 @@ public class HelpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.nav_help);
         setSupportActionBar(toolbar);
+
+        TextView textVersion = findViewById(R.id.version);
+        try {
+            String versionName = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
+            textVersion.setText("v" + versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+        }
     }
 }
